ec8b720a7e6802455f6b94530bea7224c937022e
patrizineversuxxx <r2tr@yandex.ru>
2021-12-24 18:52:47 +0400

    First commit. Working parcer and not working anything.

---
+++ src/study/ExcangeRateParser.java
@@ -0,0 +1,47 @@
+package study;
+
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.Locale;
+
+public class ExcangeRateParser {
+
+    public ExcangeRateParser() {}
+
+    public HashMap<String, Double> parse(InputStream stream, Locale locale) throws ParserConfigurationException, IOException, SAXException, ParseException {
+        var exchangeRates = new HashMap<String, Double>();
+        var factory = DocumentBuilderFactory.newInstance();
+        var builder = factory.newDocumentBuilder();
+        var document = builder.parse(stream);
+
+        var currencyList = document.getElementsByTagName("Valute");
+
+        for (int i = 0; i < currencyList.getLength(); i++) {
+            var currencyNode = currencyList.item(i);
+            var currencyElement = (Element) currencyNode;
+
+            var code = currencyElement.getElementsByTagName("CharCode").item(0);
+            var charCode = code.getTextContent();
+
+            var amountNode = currencyElement.getElementsByTagName("Nominal").item(0);
+            var amount = Integer.parseInt(amountNode.getTextContent());
+
+            var valueNode = currencyElement.getElementsByTagName("Value").item(0);
+            var stringValue = valueNode.getTextContent();
+            var format = NumberFormat.getInstance(locale);
+            var value = (Double) format.parse(stringValue) / amount;
+
+            exchangeRates.put(charCode, value);
+        }
+        System.out.println(exchangeRates.get("NOK"));
+        return exchangeRates;
+    }
+}
---
+++ src/study/Main.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class Main {
+
+
+
+    public static double forintToCrones(double forintCount) {
+        //double forintPrice = getCourseFromFile().get("forint");
+        //double cronesPrice = getCourseFromFile().get("crones");
+        //return forintPrice * forintCount / cronesPrice;
+        return 0;
+    }
+
+
+
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, ParseException {
+
+        ExcangeRateParser parser = new ExcangeRateParser();
+        FileInputStream stream = new FileInputStream("src/study/courses.xml");
+        var locale = Locale.getDefault();
+        parser.parse(stream, locale);
+        //System.out.printf("One hungrian forint costs %.2f norwegian crones.", forintToCrones(1));
+    }
+}
